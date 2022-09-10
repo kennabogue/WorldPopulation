47d5a1bb41804d47325073c5f7dd72139734c44f
kennabogue <75717592+kennabogue@users.noreply.github.com>
2022-09-10 07:51:54 -0600

    Initial Commit

---
+++ WorldPopulation/WorldPopulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldPopulationApp.swift
+//  WorldPopulation
+//
+//  Created by Kenna Bogue on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WorldPopulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
